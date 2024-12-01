c1d3f2f3ba10bc4618c53974609aab41824524de
Muhammad Ali Nasir <man171m@gmail.com>
2024-12-01 23:06:15 +0500

    added click action

--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,5 +1,10 @@
-<div class="flex flex-row justify-center p-10">
-    <button wire:click="handleClick" class="bg-white text-black font-bold text-lg rounded-4 px-10 py-5 rounded-md">
-        Click me
+<div class="flex flex-col justify-center items-center gap-5 p-10">
+
+    <h1 class="w-fit">{{ $title }}</h1>
+
+    <p class="w-fit">{{ count($users) }}</p>
+    <button wire:click="createNewUser"
+        class="w-fit bg-white text-black font-bold text-lg rounded-4 px-10 py-5 rounded-md">
+        Create New User
     </button>
 </div>
