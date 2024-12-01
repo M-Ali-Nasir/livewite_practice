07e09d6464257e2ad929196c70590cafa68f719d
Muhammad Ali Nasir <man171m@gmail.com>
2024-12-01 22:37:21 +0500

    creating first livewire component

---
+++ resources/views/livewire/clicker.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-row justify-center p-10">
+    <button wire:click="handleClick" class="bg-white text-black font-bold text-lg rounded-4 px-10 py-5 rounded-md">
+        Click me
+    </button>
+</div>
